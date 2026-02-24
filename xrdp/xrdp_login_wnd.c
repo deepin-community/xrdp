ca8ac1c3ca5936719d1040823a172ba620aafa32
hudeng <hudeng@deepin.org>
2026-02-24 14:09:25 +0800

    Fix CVE-2025-68670: Stack-based buffer overflow in domain string parsing
    - Add resultSize parameter to xrdp_wm_parse_domain_information()
    - Use resultSize instead of hardcoded 256 bytes
    - Add boundary check: if (pos > 0 && (unsigned int)pos < resultSize)
    - Use strlcpy instead of g_strncpy with proper size parameter
    - Update debian/changelog and add patch to series
    
    References:
      - CVE-2025-68670
      - GHSA-rwvg-gp87-gh6f
      - https://github.com/neutrinolabs/xrdp/commit/488c8c7d4d189514a366cd8301b6e816c5218ffa
      - https://nvd.nist.gov/vuln/detail/CVE-2025-68670

--- xrdp/xrdp_login_wnd.c
+++ xrdp/xrdp_login_wnd.c
@@ -277,7 +277,8 @@ xrdp_wm_ok_clicked(struct xrdp_bitmap *wnd)
 */
 static int
 xrdp_wm_parse_domain_information(char *originalDomainInfo, int comboMax,
-                                 int decode, char *resultBuffer)
+                                 int decode,
+                                 char *resultBuffer, unsigned int resultSize)
 {
     int ret;
     int pos;
@@ -287,8 +288,7 @@ xrdp_wm_parse_domain_information(char *originalDomainInfo, int comboMax,
     /* If the first char in the domain name is '_' we use the domain
        name as IP*/
     ret = 0; /* default return value */
-    /* resultBuffer assumed to be 256 chars */
-    g_memset(resultBuffer, 0, 256);
+    g_memset(resultBuffer, 0, resultSize);
     if (originalDomainInfo[0] == '_')
     {
         /* we try to locate a number indicating what combobox index the user
@@ -297,8 +297,8 @@ xrdp_wm_parse_domain_information(char *originalDomainInfo, int comboMax,
          * Underscore is a valid name in the domain.
          * Invalid chars are ignored in microsoft client therefore we use '_'
          * again. this sec '__' contains the split for index.*/
-        pos = g_pos(&originalDomainInfo[1], "__");
-        if (pos > 0)
+         pos = g_pos(&originalDomainInfo[1], "__");
+        if (pos > 0 && (unsigned int)pos < resultSize)
         {
             /* an index is found we try to use it */
             LOG(LOG_LEVEL_DEBUG, "domain contains index char __");
@@ -320,12 +320,12 @@ xrdp_wm_parse_domain_information(char *originalDomainInfo, int comboMax,
                 }
             }
             /* pos limit the String to only contain the IP */
-            g_strncpy(resultBuffer, &originalDomainInfo[1], pos);
+            strlcpy(resultBuffer, &originalDomainInfo[1], pos + 1);
         }
         else
         {
             LOG(LOG_LEVEL_DEBUG, "domain does not contain _");
-            g_strncpy(resultBuffer, &originalDomainInfo[1], 255);
+            strlcpy(resultBuffer, &originalDomainInfo[1], resultSize);
         }
     }
     return ret;
@@ -450,7 +450,8 @@ xrdp_wm_show_edits(struct xrdp_wm *self, struct xrdp_bitmap *combo)
                     {
                         xrdp_wm_parse_domain_information(
                             self->session->client_info->domain,
-                            combo->data_list->count, 0, resultIP);
+                            combo->data_list->count, 0,
+                            resultIP, sizeof(resultIP));
                         g_strncpy(b->caption1, resultIP, 255);
                         b->edit_pos = g_mbstowcs(0, b->caption1, 0);
                     }
@@ -875,7 +876,8 @@ xrdp_login_wnd_create(struct xrdp_wm *self)
     combo->item_index = xrdp_wm_parse_domain_information(
                             self->session->client_info->domain,
                             combo->data_list->count, 1,
-                            resultIP /* just a dummy place holder, we ignore */ );
+                            resultIP,/* just a dummy place holder, we ignore */
+                            sizeof(resultIP));
     xrdp_wm_show_edits(self, combo);
 
     return 0;
